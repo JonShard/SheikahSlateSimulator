9908fc52a88f3c57db4d34366efb30a01656118e
JonShard <jonems@stud.ntnu.no>
2018-10-28 16:14:32 +0100

    Added a visual effect to the rune activity.

--- app/src/main/java/com/example/jonshard/sheikaslatesim/RuneActivity.java
+++ app/src/main/java/com/example/jonshard/sheikaslatesim/RuneActivity.java
@@ -1,11 +1,21 @@
 package com.example.jonshard.sheikaslatesim;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.Interpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 public class RuneActivity extends AppCompatActivity {
@@ -20,6 +30,8 @@ public class RuneActivity extends AppCompatActivity {
     AccelerationListener listener;
     ImageView imageView;
 
+    ImageView effectA;
+    ImageView effectB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +43,62 @@ public class RuneActivity extends AppCompatActivity {
         int runeID = intent.getIntExtra("runeID", 0);
 
         imageView = findViewById(R.id.activity_rune_imageView);
-        if (runeID == MAGNESIS) imageView.setImageResource(R.color.runeMagnesis);
-        if (runeID == STASIS) imageView.setImageResource(R.color.runeStasis);
-        if (runeID == CRYONIS) imageView.setImageResource(R.color.runeCryonis);
+        effectA = findViewById(R.id.activity_rune_imageView_effectA);
+        effectB = findViewById(R.id.activity_rune_imageView_effectB);
+
+        if (runeID == MAGNESIS) {
+            imageView.setImageResource(R.color.runeMagnesis);
+            effectA.setColorFilter(ContextCompat.getColor(this, R.color.runeMagnesisTint), android.graphics.PorterDuff.Mode.MULTIPLY);
+            effectB.setColorFilter(ContextCompat.getColor(this, R.color.runeMagnesisTint), android.graphics.PorterDuff.Mode.MULTIPLY);
+        }
+        else if (runeID == STASIS) {
+            imageView.setImageResource(R.color.runeStasis);
+            effectA.setColorFilter(ContextCompat.getColor(this, R.color.runeStasisTint), android.graphics.PorterDuff.Mode.MULTIPLY);
+            effectB.setColorFilter(ContextCompat.getColor(this, R.color.runeStasisTint), android.graphics.PorterDuff.Mode.MULTIPLY);
+        }
+        else if (runeID == CRYONIS) {
+            imageView.setImageResource(R.color.runeCryonis);
+            effectA.setColorFilter(ContextCompat.getColor(this, R.color.runeCryonisTint), android.graphics.PorterDuff.Mode.MULTIPLY);
+            effectB.setColorFilter(ContextCompat.getColor(this, R.color.runeCryonisTint), android.graphics.PorterDuff.Mode.MULTIPLY);
+        }
+
+
+
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+
+        RotateAnimation rotateA = new RotateAnimation(0,360,1250,1250);
+        rotateA.setDuration(25000);
+        rotateA.setRepeatCount(Animation.INFINITE);
+        rotateA.setInterpolator(new Interpolator() {
+            @Override
+            public float getInterpolation(float v) {
+                return v;
+            }
+        });
+
+        effectA.setAnimation(rotateA);
+        rotateA.start();
+
+
+        RotateAnimation rotateB = new RotateAnimation(180,-180,1250,1250);
+        rotateB.setDuration(15000);
+        rotateB.setRepeatCount(Animation.INFINITE);
+        rotateB.setInterpolator(new Interpolator() {
+            @Override
+            public float getInterpolation(float v) {
+                return v;
+            }
+        });
+
+        effectB.setAnimation(rotateB);
+        rotateB.start();
+
         imageView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
