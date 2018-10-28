27b6733d5d080d3850249d283131bee3e640d86e
JonShard <jonems@stud.ntnu.no>
2018-10-28 13:46:57 +0100

    Added moto cycle zero sound effect.

--- app/src/main/java/com/example/jonshard/sheikaslatesim/SoundPlayer.java
+++ app/src/main/java/com/example/jonshard/sheikaslatesim/SoundPlayer.java
@@ -20,6 +20,7 @@ public class SoundPlayer {
     static final int PATHWAY_REVIEALED =    9;
     static final int NEW_ITEM_PICKUP =      10;
     static final int SELECTION_MADE =       11;
+    static final int MOTO_CYCLE_ZERO =      12;
 
 
 
@@ -30,7 +31,8 @@ public class SoundPlayer {
     private static float runePitch = 1f;
 
     private static SoundPool soundPool;
-    private static int[] soundID = {0,0,0,0,0,   0,0,0,0,0,   0};
+//                                  1            6            11
+    private static int[] soundID = {0,0,0,0,0,   0,0,0,0,0,   0,0};
     private static int runeSteam = 0;
 
     private static final SoundPlayer ourInstance = new SoundPlayer();
@@ -67,11 +69,13 @@ public class SoundPlayer {
             soundID[3] = soundPool.load(context, R.raw.bomb_explode,     1);
             soundID[4] = soundPool.load(context, R.raw.rune_start,       1);
             soundID[5] = soundPool.load(context, R.raw.rune_continues,   1);
-            soundID[6] = soundPool.load(context, R.raw.sword_swing,       1);
+            soundID[6] = soundPool.load(context, R.raw.sword_swing,      1);
             soundID[7] = soundPool.load(context, R.raw.save_image,       1);
             soundID[8] = soundPool.load(context, R.raw.pathway_revealed, 1);
             soundID[9] = soundPool.load(context, R.raw.new_item_pickup,  1);
             soundID[10] = soundPool.load(context, R.raw.selection_made,  1);
+            soundID[11] = soundPool.load(context, R.raw.moto_cycle,      1);
+
 
 
 
