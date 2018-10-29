e2b632fc6fd665d1a359dc1a0a6354236e0ce07f
JonShard <jonems@stud.ntnu.no>
2018-10-29 12:18:45 +0100

    Fixed RuneActivity not fullscreen.

--- app/src/main/java/com/example/jonshard/sheikaslatesim/RuneActivity.java
+++ app/src/main/java/com/example/jonshard/sheikaslatesim/RuneActivity.java
@@ -38,6 +38,8 @@ public class RuneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rune);
 
+        hideSystemUI();
+
         Intent intent = getIntent();
 
         int runeID = intent.getIntExtra("runeID", 0);
@@ -116,6 +118,25 @@ public class RuneActivity extends AppCompatActivity {
         SoundPlayer.playSound(SoundPlayer.RUNE_START);
         SoundPlayer.playSound(SoundPlayer.RUNE_CONTINUES);
 
+    }
+
+
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     @Override
